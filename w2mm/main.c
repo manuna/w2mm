543f2b61222f7291f77f79aa48bec5ef18288be5
amykhayl <amykh2007@gmail.com>
2013-08-10 22:57:00 +0300

    Initial Commit

---
+++ w2mm/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  w2mm
+//
+//  Created by Andriy Mykhaylyshyn on 8/10/13.
+//  Copyright (c) 2013 Andriy Mykhaylyshyn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
